d4285336390017dde10ac675b4cf0f52b8e5b873
sendis <sendisetiawan10902@gmail.com>
2026-05-19 09:27:46 +0700

    Initial Commit

---
+++ StandWise/StandWiseApp.swift
@@ -0,0 +1,32 @@
+//
+//  StandWiseApp.swift
+//  StandWise
+//
+//  Created by Sendi Setiawan on 19/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StandWiseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
